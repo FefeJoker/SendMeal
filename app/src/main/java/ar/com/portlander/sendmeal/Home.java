f2a7125762d75fa7353136f69f75720ffe0e57e2
Federico Comelli <FefeJoker.28@gmail.com>
2020-10-06 05:11:40 -0300

    Se reemplaza el toast de la confirmacion de un nuevo pedido por una notificacion.
    En la clase NotificationPublisher deje unas cuestiones que deberian ser resueltas.

--- app/src/main/java/ar/com/portlander/sendmeal/Home.java
+++ app/src/main/java/ar/com/portlander/sendmeal/Home.java
@@ -1,11 +1,15 @@
 package ar.com.portlander.sendmeal;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.ClipData;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,11 +24,15 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 public class Home extends AppCompatActivity {
 
     private Toolbar tb_home;
+    private NotificationChannel notificationChannel;
     private MenuItem menu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        createNotificationChannel();
 
         tb_home = (Toolbar) findViewById(R.id.toolbar_home);
         setSupportActionBar(tb_home);
@@ -65,6 +73,20 @@ public class Home extends AppCompatActivity {
         return true;
 
     }
+
+    private void createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            String channel_id = getString(R.string.channel_id);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            notificationChannel = new NotificationChannel(channel_id, name, importance);
+            notificationChannel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
     public void registrarme(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
--- app/src/main/java/ar/com/portlander/sendmeal/NotificationPublisher.java
+++ app/src/main/java/ar/com/portlander/sendmeal/NotificationPublisher.java
@@ -1,16 +1,28 @@
 package ar.com.portlander.sendmeal;
 
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import android.widget.Toast;
 
-import java.util.concurrent.TimeUnit;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 
 public class NotificationPublisher extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, intent.getStringExtra("MENSAJE"), Toast.LENGTH_SHORT).show();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "1");
+        builder.setSmallIcon(R.drawable.plato); //Buscar un iconito lindo.
+        builder.setContentTitle("SendMeal");
+        builder.setContentText(intent.getStringExtra("MENSAJE"));
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(1, builder.build());
+        //Se deberia manejar mejor el tema de los ids de las notificaciones.
     }
 }
